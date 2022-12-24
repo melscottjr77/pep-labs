d723790a2d8ce01e6f801a1430744a15cf363bd7
melscottjr77 <melscottjr77@gmail.com>
2022-12-24 22:03:33 +0000

    Saved progress on MethodParameters 2022-12-24 22:03:33 +0000

--- HelloWorld/src/main/java/HelloWorld.java
+++ HelloWorld/src/main/java/HelloWorld.java
@@ -6,6 +6,6 @@ public class HelloWorld{
      * return "Hello, world!"
      */
     public static void main(String[] args){
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world!);
     }
 }
